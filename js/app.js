e04c10f29710e14aa34921cc217c300433fa5cfb
gabrieljaquish <gabrieljaquish@gmail.com>
2019-08-27 21:13:51 -0400

    updated Javascript

--- js/app.js
+++ js/app.js
@@ -27,9 +27,12 @@ function Parallax() {
 }
 
 function noParallax() {
-    document.getElementById("no-parallax").setAttribute('display', 'block');
-    document.getElementById("parallax").setAttribute('display', 'none');
+    document.getElementById("no-parallax").style.display = "block";
 
+    const layers = document.getElementsByClassName("parallax");
+    for (let i = 0; i < layers.length; i++) {
+        layers[i].style.display = "none";
+    }
 }
 
 function SmoothScroll() {
@@ -59,102 +62,3 @@ function toParallax_or_notToParallax() {
 }
 
 document.window.onload = toParallax_or_notToParallax();
-
-
-/*
-
-function castParallax() {
-    // This Javascrtipt is modified from that used on https://www.firewatchgame.com/
-
-    //Get all the elements we're going to touch
-    const layers = document.getElementsByClassName("parallax");
-    const nav = document.getElementById("parallax");
-    const main = document.getElementById("main");
-    const moon = document.getElementById("moon");
-    const stars = document.getElementById("stars");
-
-    //get window height
-    const height = $(window).height();
-    console.log(height);
-
-    //temp vars
-    var layer, speed, yPos;
-
-    window.addEventListener("scroll", function() {
-
-        let currentScrollPosition = this.pageYOffset;
-
-        //Loops thru each MTN Layer and moves it by the speed set in the data-speed of each html element 
-        for (var i = 0; i < layers.length; i++) {
-            layer = layers[i];
-            speed = layer.getAttribute('data-speed');
-
-            yPos = ((currentScrollPosition * speed) / 100);
-
-            layer.setAttribute('style', 'transform: translate(0px, ' + yPos + 'px');
-        }
-        console.log(yPos);
-        main.setAttribute('style', 'transform: translate(0px, ' + (currentScrollPosition / 10) + 'px');
-
-        if (Math.abs(yPos) <= height) {
-            nav.css.display = "none";
-        }
-        if (Math.abs(yPos) > height) {
-            nav.css.display = "block";
-            console.log("block");
-        }
-
-    });
-
-
-}
-
-
-
-
-function dispelParallax() {
-    $("#nonparallax").css('display', 'block');
-    $("#parallax").css('display', 'none');
-}
-
-function castSmoothScroll() {
-    $.srSmoothscroll({
-        step: 80,
-        speed: 300,
-        ease: 'linear'
-    });
-}
-
-
-
-
-
-function toParallax_or_nottoParallax() {
-    if () {
-        noParallax();
-    } else if () {
-        Parallax();
-    } else {
-        Parallax();
-    }
-}
-
-
-
-function startSite() {
-
-    var platform = navigator.platform.toLowerCase();
-
-    if (platform.indexOf('ipad') != -1 || platform.indexOf('iphone') != -1) {
-        dispelParallax().srSmoothscroll();
-    } else if (platform.indexOf('win32') != -1 || platform.indexOf('linux') != -1) {
-        castParallax();
-        if ($.browser.webkit) {
-            castSmoothScroll();
-        }
-    } else {
-        castParallax();
-    }
-
-}
-*/
